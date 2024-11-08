e243294f6e0b96876a66b5d5d33d6f772db95de1
Ming Jun <evan.zhangmingjun@gmail.com>
2024-11-08 13:44:16 +0800

    feat: add sentry to nextjs frontend

---
+++ frontend/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://58466f2809b81bad68c5998cba1b4f95@o4508261001003008.ingest.de.sentry.io/4508261008146512",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
