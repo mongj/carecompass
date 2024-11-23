887a0c5ab403f849759dce34322d14430ab6d283
Ming Jun <evan.zhangmingjun@gmail.com>
2024-11-23 22:40:25 +0800

    feat: add filter for saved searches

--- frontend/src/app/profile/saved-searches/page.tsx
+++ frontend/src/app/profile/saved-searches/page.tsx
@@ -10,19 +10,27 @@ import { useAuth } from "@clerk/nextjs";
 import { Tag } from "@chakra-ui/react";
 import { ChevronRightIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import LoadingSpinner from "@/ui/loading";
+import { MultiSelect } from "@opengovsg/design-system-react";
+import { ReviewTargetType } from "@/types/review";
+import React, { useEffect, useState } from "react";
 
 export default function SavedSearchesPage() {
   const auth = useAuth();
   const router = useRouter();
 
+  const [isLoading, setIsLoading] = useState(true);
   const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+  const [selectedResourceTypes, setSelectedResourceTypes] = useState<string[]>(
+    Object.values(ReviewTargetType),
+  );
 
   useEffect(() => {
     if (!auth.userId) return;
 
     api.get(`/bookmarks?userId=${auth.userId}`).then((res) => {
       setBookmarks(res.data);
+      setIsLoading(false);
     });
   }, [auth.userId]);
 
@@ -34,10 +42,35 @@ export default function SavedSearchesPage() {
   return (
     <div className="flex h-full w-full flex-col gap-4">
       <h1 className="text-2xl font-semibold">Saved Searches</h1>
+      <div className="flex flex-col">
+        <span className="font-semibold">Filter by type</span>
+        <MultiSelect
+          values={selectedResourceTypes.map((resourceType) =>
+            resourceType.toString(),
+          )}
+          items={Object.values(ReviewTargetType).map((resourceType) => ({
+            label: ReviewTargetTypeToName(resourceType),
+            value: resourceType.toString(),
+          }))}
+          name="resourceTypes"
+          placeholder="Filter by type"
+          onChange={(values) => {
+            setSelectedResourceTypes(values);
+          }}
+        />
+      </div>
       <section className="flex flex-col gap-2">
-        {bookmarks.map((bookmark) => (
-          <BookmarkCard key={bookmark.id} bookmark={bookmark} />
-        ))}
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          bookmarks
+            .filter((bookmark) =>
+              selectedResourceTypes.includes(bookmark.targetType),
+            )
+            .map((bookmark) => (
+              <BookmarkCard key={bookmark.id} bookmark={bookmark} />
+            ))
+        )}
       </section>
     </div>
   );
